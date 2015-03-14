a45d0b2b6211f11a46077911d83279a9ed8b1ed5
Mason Glidden <mglidden@yelp.com>
2015-03-14 12:10:28 -0500

    Initial commit

---
+++ MGGStaticMapView/MGGStaticMapView.h
@@ -0,0 +1,19 @@
+//
+//  MGGStaticMapView.h
+//  MGGStaticMapView
+//
+//  Created by Mason Glidden on 3/14/15.
+//  Copyright (c) 2015 mgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGGStaticMapView.
+FOUNDATION_EXPORT double MGGStaticMapViewVersionNumber;
+
+//! Project version string for MGGStaticMapView.
+FOUNDATION_EXPORT const unsigned char MGGStaticMapViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGGStaticMapView/PublicHeader.h>
+
+
